5e92aec0bcb4b088331bf661c83d7614a6be26d7
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2019-01-14 13:25:33 +0700

    Khởi tạo

---
+++ AppStoreManagement-1612209/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppStoreManagement_1612209
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int a = 12000000;
+            a = a * 100;
+            var s = a.ToString();
+            MessageBox.Show(s);
+        }
+    }
+}
